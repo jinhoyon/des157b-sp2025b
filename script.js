ead1f31517927a22aba98ad922e8e70bb5c6b454
Jin Ho Yon <jyon@ucdavis.edu>
2025-04-29 16:31:43 -0700

    Portal Page

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
+    const image = document.querySelector('img');
     let mode = 'dark';
 
     button.addEventListener('click', function() {
@@ -12,6 +13,9 @@
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
+            button.innerHTML = 'sleep';
+            img1.removeAttribute('class'); 
+            img2.className = 'switch'; 
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -20,9 +24,12 @@
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
+            img1.className = 'switch'; 
+            img2.removeAttribute('class'); 
             for (const section of sections) {
                 section.removeAttribute('class');
             }
+            button.innerHTML = 'wake up';
             mode = 'dark'
         }
     })
